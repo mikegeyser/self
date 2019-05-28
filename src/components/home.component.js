77c0cda7875e3831e6e00b2e5beff2796985101c
Mike Geyser <mikegeyser@gmail.com>
2019-05-28 09:19:29 +0200

    Tweaked the balancing on the home component.

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -17,17 +17,14 @@ import { SharedStyles } from './shared-styles.js';
 import { profilePicture } from './svg-pictures.js';
 
 const styles = css`
-  .container {
-    margin-top: 1em;
-  }
-
   .profile-picture {
-    height: 20em;
-    width: 20em;
+    height: 25vw;
+    width: 25vw;
     overflow: hidden;
     border-radius: 20em;
     position: relative;
     float: left;
+    margin: 1em 1em 1em 0;
   }
 
   .profile-picture svg {
@@ -47,18 +44,31 @@ const styles = css`
   }
 
   h1 {
-    font-size: 4em;
+    font-size: 2em;
     margin: 1em 0 0.5em 0;
     font-family: var(--app-heading-font);
   }
 
   div.paragraph {
-    float: left;
-    width: 35em;
-    margin-left: 2em;
     padding: 0;
     text-align: justify;
     line-height: 1.8em;
+  }
+
+  @media (min-width: 460px) {
+    .profile-picture {
+      height: 20em;
+      width: 20em;
+    }
+
+    h1 {
+      font-size: 4em;
+    }
+
+    div.paragraph {
+      float: left;
+      width: 35em;
+      margin-left: 2em;
   }
 `;
 
@@ -80,22 +90,19 @@ class Home extends PageViewElement {
               Hi, I'm Mike!
             </h1>
             <p>
-              I’m a Google Developer Expert in Web Technologies from Johannesburg, South Africa,
-              where I work in the R&D team at a company called BBD. I am a co-organiser of the
-              Jozi.JS meetup group, and a frequent technical speaker. I have been hacking on the web
-              since Geocities was a 'thing', and have the keening wail of dialup modem etched into
-              my subconscious.
+              I’m a Google Developer Expert in Web Technologies from Johannesburg, South Africa, where I work in the R&D
+              team at a company called BBD. I am a co-organiser of the Jozi.JS meetup group, and a frequent technical
+              speaker. I have been hacking on the web since Geocities was a 'thing', and have the keening wail of dialup
+              modem etched into my subconscious.
             </p>
             <p>
-              While I am fluent in several ‘golden hammer’ programming languages, I have a
-              long-lived love affair with JavaScript, and cannot see its (many, obvious) flaws. I
-              have worked on lots of interesting enterprise applications, but it is the challenges
-              of the public web that really appeal to me - having spent my formative years building
-              transactional websites.
+              While I am fluent in several ‘golden hammer’ programming languages, I have a long-lived love affair with
+              JavaScript, and cannot see its (many, obvious) flaws. I have worked on lots of interesting enterprise
+              applications, but it is the challenges of the public web that really appeal to me - having spent my
+              formative years building transactional websites.
             </p>
             <p>
-              I am always eager to talk about the web platform, but be warned, I am prone to
-              hyperbole.
+              I am always eager to talk about the web platform, but be warned, I am prone to hyperbole.
             </p>
 
             <a href="https://twitter.com/mikegeyser/">@mikegeyser</a>
