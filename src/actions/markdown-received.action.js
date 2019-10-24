9773803a816070f6efbabfbf5d3fb5d32e9b4f15
Mike Geyser <mikegeyser@gmail.com>
2019-10-24 14:59:09 +0200

    Moving stuff around.

--- src/actions/markdown-received.action.js
+++
@@ -1,8 +0,0 @@
-export const MARKDOWN_RECEIVED = 'MARKDOWN_RECEIVED';
-
-export const markdownReceived = (markdown) => {
-    return {
-        type: MARKDOWN_RECEIVED,
-        markdown
-    };
-}
--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "globDirectory": "build/es5-bundled/",
-  "globPatterns": [
-    "**/*.{xml,ico,svg,html,js}"
-  ],
-  "swDest": "build/es5-bundled/sw.js"
-};
